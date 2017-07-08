44df2b9e8de7470caae6465c5dea275c8ac37d5a
Robert Edwards <edwards@jlab.org>
2017-07-08 15:44:22 -0400

    dbdisco runs and can read in an existing file.

--- dbdisco_to_graph.nim
+++ dbdisco_to_graph.nim
@@ -17,7 +17,7 @@ proc printBin(x:string): string =
 
 when isMainModule:
   ## Convenience function to open a SDB
-  echo "Declare conf db"
+  echo "Declare disco conf odb"
   var odb = newConfDataStoreDB()
 
   # Open a file
@@ -39,36 +39,39 @@ when isMainModule:
   var meta = odb.getUserdata()
   echo "it did not blowup..."
   #echo " meta= ", meta
-  #let nrow = [32, 32, 32, 96
+  let nrow = [32, 32, 32, 96]
+  let mom2_max = 0
 
   # Read all the keys
-  echo "try getting all the deserialized keys"
-  #let des_keys = allKeys[DiscoKeyOperator_t](odb)
-  let des_keys = allBinaryKeys(odb)
-  echo "found num all keys= ", des_keys.len
-  echo "here is the first all key: len= ", des_keys.len, "  val= ", printBin(des_keys[8])
-  #echo "here is the first all key: len= ", des_keys.len, "  val= ", des_keys[0]
-  echo "here are 10 of the keys"
-  #for i in 0..des_keys.len-1:
-  for i in 0..9:
-    echo "k[",i,"]= ", printBin(des_keys[i]), " len= ", des_keys[i].len
-    #echo "k[",i,"]= ", des_keys[i]
+  var bin_keys: seq[string]
+  if false:
+    echo "try getting all the deserialized keys"
+    #let des_keys = allKeys[DiscoKeyOperator_t](odb)
+    bin_keys = allBinaryKeys(odb)
+    echo "found num all keys= ", bin_keys.len
+    echo "here is the first all key: len= ", bin_keys.len, "  val= ", printBin(bin_keys[8])
+    #echo "here is the first all key: len= ", bin_keys.len, "  val= ", bin_keys[0]
+    echo "here are 10 of the keys"
+    #for i in 0..bin_keys.len-1:
+    for i in 0..9:
+      echo "k[",i,"]= ", printBin(bin_keys[i]), " len= ", bin_keys[i].len
+      #echo "k[",i,"]= ", bin_keys[i]
 
   # Test
-  if true:
+  if false:
     echo "Convert a binary key to a struct"
-    let foobar = deserializeBinary[DiscoKeyOperator_t](des_keys[8])
+    let foobar = deserializeBinary[DiscoKeyOperator_t](bin_keys[8])
     echo "foobar= ", foobar
-  
+
   # Now try
-  if true:
+  if false:
     echo "\n\nNow try values"
     var val0: DiscoValOperator_t
     var sval: string
-    var ret = getBinary(odb, des_keys[8], sval)
+    var ret = getBinary(odb, bin_keys[8], sval)
     echo "got ret= ", ret
     echo "sval = ", printBin(sval)
-    ret = get(odb, deserializeBinary[DiscoKeyOperator_t](des_keys[8]), val0)
+    ret = get(odb, deserializeBinary[DiscoKeyOperator_t](bin_keys[8]), val0)
     echo "get ret= ", ret
     echo "val0= ", val0
 
@@ -76,11 +79,10 @@ when isMainModule:
   #assert(odb.close() == 0)
 
 
-#[
   #
   # New file
   #
-  echo "Declare conf db"
+  echo "Declare redstar conf ndb"
   var ndb = newConfDataStoreDB()
 
   # Let us write a file
@@ -93,7 +95,7 @@ when isMainModule:
     echo "Does not exist"
 
   # Meta-data
-  echo "Here is the metadata to insert:\n", meta
+  #echo "Here is the metadata to insert:\n", meta
   ndb.setMaxUserInfoLen(meta.len)
 
   echo "open the ndb = ", single_file
@@ -108,25 +110,43 @@ when isMainModule:
     quit("strerror= " & $strerror(errno))
  
   # Build up some keys and associated values in a table
-  var kv = initTable[DiscoKeyOperator_t,float]()
+  #var kv = initTable[DiscoKeyOperator_t,float]()
 
-  for ll in items(labels):
-    for t_slice in 9..10:
-      for sl in 0..3:
-        for sr in 0..3:
-          #echo "t_slice= ", t_slice, " sl= ", sl, " sr= ", sr
-          let key = KeyPropElementalOperator_t(t_slice: cint(t_slice), t_source: 5, 
-                                               spin_l: cint(sl), spin_r: cint(sr), 
-                                               mass_label: SerialString(ll))
-          let val = random(3.0)  # some arbitrary number
-          kv.add(key,val)
+  # parameters
+  var disps  = [[cshort(0)]]
+  var disps2 = [[cshort(1)], [cshort(2)], [cshort(3)], [cshort(4)], [cshort(-1)], [cshort(-2)], [cshort(-3)], [cshort(-4)]]
 
-    # insert the entire table
-    ret = db.insert(kv)
-    if ret != 0:
-      echo "Ooops, ret= ", ret
-      quit("Error in insertion")
+  # Loop over all possible keys
+  for px in -mom2_max..mom2_max:
+    for py in -mom2_max..mom2_max:
+      for pz in -mom2_max..mom2_max:
+        let mom2 = px*px+py*py+pz*pz
+        if mom2 > mom2_max: continue
+        let cmom = [cshort(px), cshort(py), cshort(pz)]
 
-    # Close
-    require(db.close() == 0)
-]#    
+        for disp in items(disps):
+          echo "disp= ", @disp
+
+          #for t_slice in 0..nrow[3]-1:
+          for t_slice in 0..2:
+            #echo "t_slice= ", t_slice, " disp= ", disp, " mom= ", cmom
+            let key = DiscoKeyOperator_t(t_slice: cushort(t_slice), mom: @cmom,
+                                         disp: @disp, ndisp: cushort(disp.len))
+            echo "key= ", key
+
+            # val
+            echo "get the val"
+            var val: DiscoValOperator_t
+            let ret = get(odb, key, val)
+            assert(ret == 0)
+            let op = val.op
+            echo "val: len= ", op.len, "  op= ", op
+
+
+
+  # insert the entire table
+  #ret = db.insert(kv)
+  #assert(ret == 0)
+
+  # Close
+  assert(ndb.close() == 0)
