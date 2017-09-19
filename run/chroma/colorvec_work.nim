5a6e696d24c893a04cb8f1e66a892e69fa0e7449
Robert Edwards <edwards@jlab.org>
2017-09-19 16:11:17 -0400

    Starting to build up some data structures.

--- run/chroma/colorvec_work.nim
+++ run/chroma/colorvec_work.nim
@@ -4,6 +4,8 @@
 import os, ospaths, osproc, strutils
 import re
 import config
+import serializetools/serializexml, niledb
+import xmltree
 
 #------------------------------------------------------------------------
 # Find a local path to a file, or cache_cp it
@@ -162,7 +164,7 @@ proc gzip*(file: string): string =
 
 
 #------------------------------------------------------------------------
-proc extractLattSize*(data: string): array[0..3,int] =
+proc extractLattSize*(data: string): array[4,int] =
   ## Determine the lattice size
   # Yuk, do some file name surgery
   var stem = data.replace(re"\..*$")
@@ -173,7 +175,7 @@ proc extractLattSize*(data: string): array[0..3,int] =
   let F  = stem.split('_')
   let Ls = parseInt(F[1])
   let Lt = parseInt(F[2])
-  result = @[Ls, Ls, Ls, Lt]
+  result = [Ls, Ls, Ls, Lt]
 
 
 
@@ -184,22 +186,50 @@ proc getTimeOrigin*(Lt: int, trajj: string): int =
 
   # Seed the rng with the traj number
   let (outp, errC) = execCmdEx("t_origin.pl " & $Lt & " " & traj)
+  if errC != 0:
+    quit("Error running t_origin.pl")
 
   result = parseInt(outp)
 
 
 
 #------------------------------------------------------------------------
-proc print_header_xml*(input: string)  =
+type
+  InlineMeasurements_t* = object   
+    ## All inline measurements
+    meas:    seq[string]   # measurements
+
+
+#------------------------------------------------------------------------
+type
+  ChromaParam_t* = object   
+    ## All inline measurements
+    InlineMeasurements*:  InlineMeasurements_t      ## Yup, the inline measurements
+    nrow*:                array[4,int]              ## lattice size
+
+
+#------------------------------------------------------------------------
+type
+  Cfg_t* = object   
+    ## Configuration params
+    cfg_type*:      string              ## Type
+    cfg_file*:      string              ## File name, if it exists
+    parallel_io*:   bool                ## Whether we can use parallel io
+
+
+#------------------------------------------------------------------------
+type
+  Chroma_t* = object   
+    ## All parameters for chroma
+    Param*:         ChromaParam_t       ## Type
+    Cfg*:           Cfg_t               ## File name, if it exists
+
+
+
+#------------------------------------------------------------------------
+proc print_header_xml*(): string  =
   ## Header
-  writeFile(input, """
-<?xml version="1.0"?>
-
-<chroma>
-<Param> 
-  <InlineMeasurements>
-""")
-
+  return """<?xml version="1.0"?>"""
 
 
 #------------------------------------------------------------------------
@@ -225,7 +255,7 @@ proc print_trailer_xml*(gauge_type, gauge_cfg, input: string, lattSize: seq[int]
 
 #------------------------------------------------------------------------
 #
-proc print_harom_header_xml*(input: string) =
+proc print_harom_header_xml*(input: string): string =
   ## Header
   writeFile(input, """
 <?xml version="1.0"?>
@@ -239,7 +269,7 @@ proc print_harom_header_xml*(input: string) =
 #------------------------------------------------------------------------
 # Trailer
 #
-proc print_harom_trailer_xml*(input: string, lattSize: seq[int]) = 
+proc print_harom_trailer_xml*(input: string, lattSize: seq[int]): string =
   ## Trailer
   var file: FILE
   if not open(file, input, fmAppend):
@@ -250,3 +280,421 @@ proc print_harom_trailer_xml*(input: string, lattSize: seq[int]) =
 </Param> 
 </harom>
 """)
+
+
+#[
+
+#------------------------------------------------------------------------------
+proc print_prop_distillation_cpu(local_src_file, local_prop_file, m_q, t0_val, input): string =
+# Print propagator and matelem driver for a cpu
+
+  # Displace the origin
+  my $t0_string = ($t0_val + $HMCParam::origin) % $HMCParam::nrow[3];
+
+# Print for a single mass
+  open(INPUT, ">> $input");
+  print INPUT <<EOF;
+    <elem>
+      <Name>PROP_DISTILLATION</Name>
+      <Frequency>${HMCParam::sf_freq}</Frequency>
+      <Param>
+        <Contractions>
+          <mass_label>U${m_q}</mass_label>
+          <num_vecs>${HMCParam::num_vecs}</num_vecs>
+          <t_sources>${t0_string}</t_sources>
+          <Nt_forward>${HMCParam::Nt_forward}</Nt_forward>
+          <Nt_backward>${HMCParam::Nt_backward}</Nt_backward>
+          <decay_dir>3</decay_dir>
+        </Contractions>
+        <Propagator>
+          <version>10</version>
+          <quarkSpinType>FULL</quarkSpinType>
+          <obsvP>false</obsvP>
+          <numRetries>1</numRetries>
+          <FermionAction>
+            <FermAct>CLOVER</FermAct>
+            <Mass>${m_q}</Mass>
+            <clovCoeffR>${c_s}</clovCoeffR>
+            <clovCoeffT>${c_t}</clovCoeffT>
+            <AnisoParam>
+              <anisoP>${HMCParam::anisoP}</anisoP>
+              <t_dir>${HMCParam::t_dir}</t_dir>
+              <xi_0>${HMCParam::xi_0}</xi_0>
+              <nu>${HMCParam::nu}</nu>
+            </AnisoParam>
+            <FermState>
+              <Name>STOUT_FERM_STATE</Name>
+              <rho>${HMCParam::rho}</rho>
+              <n_smear>${HMCParam::n_smear}</n_smear>
+	      <orthog_dir>${HMCParam::orthog_dir}</orthog_dir>
+              <FermionBC>
+                <FermBC>SIMPLE_FERMBC</FermBC>
+                <boundary>1 1 1 -1</boundary>
+              </FermionBC>
+            </FermState>
+          </FermionAction>
+          <InvertParam>
+            <invType>${HMCParam::SolverType}</invType>
+            <MaxBiCGStab>${HMCParam::MaxCG}</MaxBiCGStab>
+            <RsdBiCGStab>${HMCParam::RsdCG}</RsdBiCGStab>
+          </InvertParam>
+         </Propagator>
+      </Param>
+      <NamedObject>
+        <save_solnP>true</save_solnP>
+        <gauge_id>default_gauge_field</gauge_id>
+        <distillution_id>dist_obj</distillution_id>
+        <src_file>${local_src_file}</src_file>
+        <soln_file>${local_prop_file}</soln_file>
+      </NamedObject>
+    </elem>
+
+EOF
+close(INPUT);
+}
+
+#------------------------------------------------------------------------------
+# Print propagator and matelem driver for a cpu
+#
+sub print_prop_distillation_cpu_mg
+{
+  local($local_eig_file, $local_prop_file, $m_q, $t0_val, $input) = @_;
+
+  # Displace the origin
+  my $t0_string = ($t0_val + $HMCParam::origin) % $HMCParam::nrow[3];
+
+# Print for a single mass
+  open(INPUT, ">> $input");
+  print INPUT <<EOF;
+    <elem>
+      <Name>PROP_AND_MATELEM_DISTILLATION</Name>
+      <Frequency>${HMCParam::sf_freq}</Frequency>
+      <Param>
+        <Contractions>
+          <mass_label>U${m_q}</mass_label>
+          <num_vecs>${HMCParam::num_vecs}</num_vecs>
+          <t_sources>${t0_string}</t_sources>
+          <Nt_forward>${HMCParam::Nt_forward}</Nt_forward>
+          <Nt_backward>${HMCParam::Nt_backward}</Nt_backward>
+          <decay_dir>3</decay_dir>
+          <num_tries>${HMCParam::num_tries}</num_tries>
+        </Contractions>
+        <Propagator>
+          <version>10</version>
+          <quarkSpinType>FULL</quarkSpinType>
+          <obsvP>false</obsvP>
+          <numRetries>1</numRetries>
+          <FermionAction>
+            <FermAct>UNPRECONDITIONED_CLOVER</FermAct>
+            <Mass>${m_q}</Mass>
+            <clovCoeffR>${c_s}</clovCoeffR>
+            <clovCoeffT>${c_t}</clovCoeffT>
+            <AnisoParam>
+              <anisoP>${HMCParam::anisoP}</anisoP>
+              <xi_0>${HMCParam::xi_0}</xi_0>
+              <nu>${HMCParam::nu}</nu>
+              <t_dir>${HMCParam::t_dir}</t_dir>
+            </AnisoParam>
+            <FermState>
+              <Name>STOUT_FERM_STATE</Name>
+              <rho>${HMCParam::rho}</rho>
+              <n_smear>${HMCParam::n_smear}</n_smear>
+	      <orthog_dir>${HMCParam::orthog_dir}</orthog_dir>
+              <FermionBC>
+                <FermBC>SIMPLE_FERMBC</FermBC>
+                <boundary>1 1 1 -1</boundary>
+              </FermionBC>
+            </FermState>
+          </FermionAction>
+          <InvertParam>
+            <invType>QOP_CLOVER_MULTIGRID_INVERTER</invType>
+            <Mass>${m_q}</Mass>
+            <Clover>${c_s}</Clover>
+            <CloverT>${c_t}</CloverT>
+            <AnisoXi>${HMCParam::xi_0}</AnisoXi>
+            <AnisoNu>${HMCParam::nu}</AnisoNu>
+            <MaxIter>200</MaxIter>
+            <Residual>${HMCParam::RsdCG}</Residual>
+            <Verbose>0</Verbose>
+            <Levels>2</Levels>
+            <Blocking>
+              <elem>3 3 3 8</elem>
+              <elem>2 2 2 4</elem>
+            </Blocking>
+
+            <NumNullVecs>24 32</NumNullVecs>
+            <NumExtraVecs>0 0</NumExtraVecs>
+
+            <NullResidual>0.4 0.4</NullResidual>
+            <NullMaxIter>100 100</NullMaxIter>
+            <NullConvergence>0.5 0.5</NullConvergence>
+
+            <Underrelax>1.0 1.0</Underrelax>
+            <NumPreHits>0 0</NumPreHits>
+            <NumPostHits>4 1</NumPostHits>
+            <CoarseMaxIter>12 12</CoarseMaxIter>
+            <CoarseResidual>0.2 0.3</CoarseResidual>
+          </InvertParam>
+         </Propagator>
+      </Param>
+      <NamedObject>
+        <gauge_id>default_gauge_field</gauge_id>
+        <colorvec_files>${local_eig_file}</colorvec_files>
+        <prop_op_file>${local_prop_file}</prop_op_file>
+      </NamedObject>
+    </elem>
+
+EOF
+close(INPUT);
+}
+
+
+#------------------------------------------------------------------------------
+# Print propagator and matelem driver for a gpu
+#
+sub print_prop_distillation_gpu_mg
+{
+  local($local_eig_file, $local_prop_file, $m_q, $t0_val, $input) = @_;
+
+  # Displace the origin
+  my $t0_string = ($t0_val + $HMCParam::origin) % $HMCParam::nrow[3];
+
+# Print for a single mass
+  open(INPUT, ">> $input");
+  print INPUT <<EOF;
+    <elem>
+      <Name>PROP_AND_MATELEM_DISTILLATION</Name>
+      <Frequency>${HMCParam::sf_freq}</Frequency>
+      <Param>
+        <Contractions>
+          <mass_label>U${m_q}</mass_label>
+          <num_vecs>${HMCParam::num_vecs}</num_vecs>
+          <t_sources>${t0_string}</t_sources>
+          <Nt_forward>${HMCParam::Nt_forward}</Nt_forward>
+          <Nt_backward>${HMCParam::Nt_backward}</Nt_backward>
+          <decay_dir>3</decay_dir>
+          <num_tries>${HMCParam::num_tries}</num_tries>
+        </Contractions>
+        <Propagator>
+          <version>10</version>
+          <quarkSpinType>FULL</quarkSpinType>
+          <obsvP>false</obsvP>
+          <numRetries>1</numRetries>
+          <FermionAction>
+            <FermAct>CLOVER</FermAct>
+            <Mass>${m_q}</Mass>
+            <clovCoeffR>${c_s}</clovCoeffR>
+            <clovCoeffT>${c_t}</clovCoeffT>
+            <AnisoParam>
+              <anisoP>${HMCParam::anisoP}</anisoP>
+              <xi_0>${HMCParam::xi_0}</xi_0>
+              <nu>${HMCParam::nu}</nu>
+              <t_dir>${HMCParam::t_dir}</t_dir>
+            </AnisoParam>
+            <FermState>
+              <Name>STOUT_FERM_STATE</Name>
+              <rho>${HMCParam::rho}</rho>
+              <n_smear>${HMCParam::n_smear}</n_smear>
+	      <orthog_dir>${HMCParam::orthog_dir}</orthog_dir>
+              <FermionBC>
+                <FermBC>SIMPLE_FERMBC</FermBC>
+                <boundary>1 1 1 -1</boundary>
+              </FermionBC>
+            </FermState>
+          </FermionAction>
+
+          <InvertParam>
+            <invType>QUDA_MULTIGRID_CLOVER_INVERTER</invType>
+
+            <MULTIGRIDParams>
+              <Residual>2.5e-1</Residual>
+              <CycleType>MG_RECURSIVE</CycleType>
+              <RelaxationOmegaMG>1.0</RelaxationOmegaMG>
+              <RelaxationOmegaOuter>1.0</RelaxationOmegaOuter>
+              <MaxIterations>10</MaxIterations> 
+              <SmootherType>MR</SmootherType>
+              <Verbosity>false</Verbosity>
+              <Precision>HALF</Precision>
+              <Reconstruct>RECONS_8</Reconstruct>
+              <NullVectors>24 32</NullVectors>
+              <GenerateNullspace>true</GenerateNullspace>
+              <GenerateAllLevels>true</GenerateAllLevels>
+              <Pre-SmootherApplications>4 4</Pre-SmootherApplications>
+              <Post-SmootherApplications>4 4</Post-SmootherApplications>
+              <SchwarzType>ADDITIVE_SCHWARZ</SchwarzType>
+              <Blocking>
+                <elem>3 3 3 4</elem>
+                <elem>2 2 2 2</elem>
+              </Blocking>
+            </MULTIGRIDParams>
+
+            <RsdTarget>${HMCParam::RsdCG}</RsdTarget>
+            <CloverParams>
+              <Mass>${m_q}</Mass>
+              <clovCoeffR>${c_s}</clovCoeffR>
+              <clovCoeffT>${c_t}</clovCoeffT>
+              <AnisoParam>
+                <anisoP>true</anisoP>
+                <t_dir>3</t_dir>
+                <xi_0>${HMCParam::xi_0}</xi_0>
+                <nu>${HMCParam::nu}</nu>
+              </AnisoParam>
+            </CloverParams>
+            <Delta>1.0e-4</Delta>
+            <MaxIter>200</MaxIter>
+            <RsdToleranceFactor>100</RsdToleranceFactor>
+            <SilentFail>true</SilentFail>
+            <AntiPeriodicT>true</AntiPeriodicT>
+            <SolverType>GCR</SolverType>
+            <Verbose>true</Verbose>
+            <AsymmetricLinop>false</AsymmetricLinop>
+            <CudaReconstruct>RECONS_12</CudaReconstruct>
+            <CudaSloppyPrecision>SINGLE</CudaSloppyPrecision>
+            <CudaSloppyReconstruct>RECONS_8</CudaSloppyReconstruct>
+            <AxialGaugeFix>false</AxialGaugeFix>
+            <AutotuneDslash>false</AutotuneDslash>
+            <SubspaceID>foo</SubspaceID>
+          </InvertParam>
+
+         </Propagator>
+      </Param>
+      <NamedObject>
+        <gauge_id>default_gauge_field</gauge_id>
+        <colorvec_files>${local_eig_file}</colorvec_files>
+        <prop_op_file>${local_prop_file}</prop_op_file>
+      </NamedObject>
+    </elem>
+
+EOF
+close(INPUT);
+}
+
+
+#------------------------------------------------------------------------------
+# Print propagator and matelem driver for a avx
+#
+sub print_prop_distillation_avx
+{
+  local($local_src_file, $local_prop_file, $m_q, $t0_val, $input) = @_;
+
+  # Displace the origin
+  my $t0_string = ($t0_val + $HMCParam::origin) % $HMCParam::nrow[3];
+
+# Print for a single mass
+  open(INPUT, ">> $input");
+  print INPUT <<EOF;
+    <elem>
+      <Name>PROP_DISTILLATION</Name>
+      <Frequency>${HMCParam::sf_freq}</Frequency>
+      <Param>
+        <Contractions>
+          <mass_label>U${m_q}</mass_label>
+          <num_vecs>${HMCParam::num_vecs}</num_vecs>
+          <t_sources>${t0_string}</t_sources>
+          <Nt_forward>${HMCParam::Nt_forward}</Nt_forward>
+          <Nt_backward>${HMCParam::Nt_backward}</Nt_backward>
+          <decay_dir>3</decay_dir>
+        </Contractions>
+        <Propagator>
+          <version>10</version>
+          <quarkSpinType>FULL</quarkSpinType>
+          <obsvP>false</obsvP>
+          <numRetries>1</numRetries>
+          <FermionAction>
+            <FermAct>CLOVER</FermAct>
+            <Mass>${m_q}</Mass>
+            <clovCoeffR>${c_s}</clovCoeffR>
+            <clovCoeffT>${c_t}</clovCoeffT>
+            <AnisoParam>
+              <anisoP>${HMCParam::anisoP}</anisoP>
+              <t_dir>${HMCParam::t_dir}</t_dir>
+              <xi_0>${HMCParam::xi_0}</xi_0>
+              <nu>${HMCParam::nu}</nu>
+            </AnisoParam>
+            <FermState>
+              <Name>STOUT_FERM_STATE</Name>
+              <rho>${HMCParam::rho}</rho>
+              <n_smear>${HMCParam::n_smear}</n_smear>
+	      <orthog_dir>${HMCParam::orthog_dir}</orthog_dir>
+              <FermionBC>
+                <FermBC>SIMPLE_FERMBC</FermBC>
+                <boundary>1 1 1 -1</boundary>
+              </FermionBC>
+            </FermState>
+          </FermionAction>
+         <InvertParam>
+           <invType>INTEL_CLOVER_INVERTER</invType>
+           <SolverType>BICGSTAB</SolverType>
+           <AntiPeriodicT>true</AntiPeriodicT>
+           <Compress>true</Compress>
+           <Verbose>true</Verbose>
+           <MaxIter>${HMCParam::MaxCG}</MaxIter>
+           <RsdTarget>${HMCParam::RsdCG}</RsdTarget>
+           <CloverParams>
+             <Mass>${m_q}</Mass>
+             <clovCoeffR>${c_s}</clovCoeffR>
+             <clovCoeffT>${c_t}</clovCoeffT>
+             <AnisoParam>
+               <anisoP>${HMCParam::anisoP}</anisoP>
+               <t_dir>${HMCParam::t_dir}</t_dir>
+               <xi_0>${HMCParam::xi_0}</xi_0>
+               <nu>${HMCParam::nu}</nu>
+             </AnisoParam>
+           </CloverParams>
+           <NCores>15</NCores>
+           <By>8</By>
+           <Bz>8</Bz>
+           <Sy>1</Sy>
+           <Sz>1</Sz>
+           <PadXY>1</PadXY>
+           <PadXYZ>0</PadXYZ>
+           <SOALEN>8</SOALEN>
+           <VECLEN>16</VECLEN>
+           <MinCt>2</MinCt>
+           <RsdToleranceFactor>50</RsdToleranceFactor>
+           <Tune>true</Tune>
+         </InvertParam>
+         </Propagator>
+      </Param>
+      <NamedObject>
+        <save_solnP>true</save_solnP>
+        <gauge_id>default_gauge_field</gauge_id>
+        <distillution_id>dist_obj</distillution_id>
+        <src_file>${local_src_file}</src_file>
+        <soln_file>${local_prop_file}</soln_file>
+      </NamedObject>
+    </elem>
+
+EOF
+close(INPUT);
+}
+
+
+
+
+]#
+
+
+#------------------------------------------------------------------------------
+when isMainModule:
+  let 
+    foo = "hello"
+    bar = "there"
+
+  let cfg_file = "/lustre/atlas/proj-shared/nph103/szscl21_24_256_b1p50_t_x4p300_um0p0850_sm0p0743_n1p265/cfgs/szscl21_24_256_b1p50_t_x4p300_um0p0850_sm0p0743_n1p265_cfg_1140d.lime"
+
+  let inline = InlineMeasurements_t(meas: @[foo, bar])
+  echo "inline:\n", inline
+  let xml = serializeXML(inline)
+  echo "xml:\n", xml
+
+  let stem = "szscl21_24_256_b1p50_t_x4p300_um0p0850_sm0p0743_n1p265"
+  let lattSize = extractLattSize(stem)
+
+  var chromaParam = ChromaParam_t(nrow: lattSize, InlineMeasurements: inline)
+  echo "Param:\n", $serializeXML(chromaParam)
+
+  let cfg = Cfg_t(cfg_type: "SCIDAC", cfg_file: cfg_file, parallel_io: true)
+
+  let Chroma = Chroma_t(Param: chromaParam, Cfg: cfg)
+  echo "Chroma:\n", $serializeXML(Chroma)
