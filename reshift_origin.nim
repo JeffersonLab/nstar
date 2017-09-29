07527a1a1ba0cd971333010f64edfca718a140bf
Robert Edwards <edwards@jlab.org>
2017-09-29 17:16:32 -0400

    First version.

--- reshift_origin.nim
+++ reshift_origin.nim
@@ -4,25 +4,40 @@
 ## a bogus t_origin=74, and will rename them with the proper time-origin based on the
 ## drand48 RNG version of t_origin.
 
-import prop_elem_type, niledb
+import prop_elem_type, niledb, drand48
 import serializetools/serializebin, serializetools/serialstring, serializetools/array2d
-import strutils, posix, os, hashes, complex
+import strutils, posix, os, hashes, complex, re
   
-proc openOldDB(out_file: string): ConfDataStoreDB =
-  ## Convenience function to open a DB
-  echo "Open the db = ", out_file
-  result = newConfDataStoreDB()
-  let ret = result.open(out_file, O_RDONLY, 0o400)
-  assert(ret == 0)
+#------------------------------------------------------------------------
+proc extractLattSize(data: string): array[4,int] =
+  ## Determine the lattice size
+  # Yuk, do some file name surgery
+  var stem = data.replace(re"\..*$")
+  stem = stem.replace(re"per\..*$")
+  stem = stem.replace(re"non\..*$")
+  stem = stem.replace(re"dir\..*$")
 
-proc openNewDB(out_file: string): ConfDataStoreDB =
-  ## Convenience function to open a DB
-  echo "Write the db = ", out_file
-  result = newConfDataStoreDB()
-  let ret = result.open(out_file, O_WRONLY or O_TRUNC, 0o644)
-  assert(ret == 0)
+  let F  = stem.split('_')
+  let Ls = parseInt(F[1])
+  let Lt = parseInt(F[2])
+  result = [Ls, Ls, Ls, Lt]
 
 
+proc getTimeOrigin(Lt: int, trajj: string): int =
+  ## Displace the origin of the time slices using the trajectory as a seed to a RNG
+  # Clean out characters and set the rng with the traj number as an int
+  var traj = parseInt(trajj.replace(re"[a-zA-Z]"))
+  srand48(traj)
+  
+  # Clean out rngs
+  for i in 1..20:
+    discard drand48()
+
+  # Origin is in the interval [0,Lt-1)
+  result = int(float(Lt)*drand48())
+
+
+#------------------------------------------------------------------------
 when isMainModule:
   # Open the DB
   echo "paramCount = ", paramCount()
@@ -57,29 +72,25 @@ when isMainModule:
     for t0 in 0..Lt-1:
       # Has to be a sensible filename
       let infile  = indir & "/" & long_stem & ".t0_" & $t0 & ".sdb" & seqno
-      let outfile = outdir & "/" & sdb
       echo "  input file= ", infile
       if not fileExists(infile): continue
 
       # Quick sanity check
-      if getFileSize(infile) == 0:
-        echo "CHECK_PROP: bad   ", sdb
-        continue
+      assert(getFileSize(infile) > 0)
 
       # Read all the keys/vals
       var old_db = newConfDataStoreDB()
       assert(old_db.open(infile, O_RDONLY, 0o400) == 0)
-      let meta_data = old_db.getUserdata()
 
       type K = KeyPropElementalOperator_t
       let all_keys = allKeys[K](old_db)
       echo "found num keys= ", all_keys.len
-      old_db.close()
+      discard close(old_db)
 
       # Only need to check one of the keys, find its origin and decide on the new name
       let first_key = all_keys[0]
 
-      let new_t_source = all_keys[0].t_source - 74 + t_origin
+      let new_t_source = first_key.t_source - 74 + t_origin
       let outfile = outdir & "/" & long_stem & ".t0_" & $new_t_source & ".sdb" & seqno
       echo "new file= ", outfile
 
