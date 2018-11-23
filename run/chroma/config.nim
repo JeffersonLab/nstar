ab55da8d30fcb0553ea0c724556869f1350859f1
Robert Edwards <edwards@jlab.org>
2018-11-23 10:08:32 -0800

    Switched back the proj dir.

--- run/chroma/config.nim
+++ run/chroma/config.nim
@@ -2,7 +2,7 @@
 
 #import strutils, os, ospaths
 
-proc getBase(): string = return "/project/projectdirs/m2156"
+proc getBase(): string = return "/global/homes/r/redwards/scratch"
 #proc getBase(): string = return "./"
 proc getStem*(): string = return "szscl21_24_256_b1p50_t_x4p300_um0p0850_sm0p0743_n1p265"
 proc getNumVecs*(): int = 162
