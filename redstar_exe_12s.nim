b527b1df2c516be9fddd783d20ea464cbc43f1a7
Robert Edwards <edwards@jlab.org>
2019-01-02 01:33:15 -0500

    Tweaks.

--- redstar_exe_12s.nim
+++ redstar_exe_12s.nim
@@ -3,7 +3,7 @@
 import redstar_exes
 
 proc redstar_exe*(): Exes_t =
-  ## Return executables for nstar
+  ## Return executables for 12s
   result.redstar_gen_graph     = "/Users/edwards/Documents/qcd/git/devel/redstar/build/src/redstar_gen_graph"
   result.redstar_npt           = "/Users/edwards/Documents/qcd/git/devel/redstar/build/src/redstar_npt"
   result.smeared_hadron_node   = "/Users/edwards/Documents/qcd/git/devel/colorvec/build/src/hadron_node"
